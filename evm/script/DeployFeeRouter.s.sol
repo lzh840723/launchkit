cf758c67c85738eccb74845b39784398062919ea
lzh <lzh840723@hotmail.com>
2025-09-01 18:31:43 +0900

    Day5: TokenFactory guards+event; tests; Script import lint; SimpleERC20; Evidence Pack #1

--- evm/script/DeployFeeRouter.s.sol
+++ evm/script/DeployFeeRouter.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-import "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 import "../contracts/TokenFactory.sol";
 
 contract DeployFactory is Script {
